b26127de24fe91eda8f3e026840b20e4c781b539
Patricio Mena <luispatricio.mh@gmail.com>
2025-01-22 23:35:19 -0600

    Add services carousel

---
+++ src/components/sections/ServicesCarousel.tsx
@@ -0,0 +1,56 @@
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+
+import dog from '../../assets/dog.jpg';
+import pens from '../../assets/pens.jpg';
+import bottles from '../../assets/bottles.jpg';
+import dogWebp from '../../assets/dog.webp';
+import pensWebp from '../../assets/pens.webp';
+import bottlesWebp from '../../assets/bottles.webp';
+
+const carouselImages = [
+  { jpg: dog, webp: dogWebp, alt: 'Image of a dog with toys' },
+  { jpg: pens, webp: pensWebp, alt: ' Image of pens on a desk' },
+  {
+    jpg: bottles,
+    webp: bottlesWebp,
+    alt: 'Image of water plastic bottles',
+  },
+];
+
+function ServicesCarousel() {
+  return (
+    <Carousel
+      className="rounded-3xl overflow-hidden "
+      opts={{
+        loop: true,
+      }}
+      plugins={[
+        Autoplay({
+          delay: 4000,
+        }),
+      ]}>
+      <CarouselContent>
+        {carouselImages.map(({ jpg, webp, alt }, index) => (
+          <CarouselItem key={index}>
+            <picture>
+              <source srcSet={webp.src} type="image/webp" />
+              <source srcSet={jpg.src} type="image/jpeg" />
+              <img
+                src={jpg.src}
+                alt={alt}
+                className="w-full h-full max-h-[448px] object-cover rounded-3xl "
+              />
+            </picture>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+    </Carousel>
+  );
+}
+
+export default ServicesCarousel;
