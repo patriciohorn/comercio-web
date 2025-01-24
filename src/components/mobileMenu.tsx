dcbfab190745cae18f278f916517f985182874eb
Patricio Mena <luispatricio.mh@gmail.com>
2025-01-23 21:07:41 -0600

    Test IOS sheet issue

--- src/components/mobileMenu.tsx
+++ src/components/mobileMenu.tsx
@@ -11,33 +11,26 @@ import logo from '../assets/comweb-logo-outline.png';
 import { Link } from './ui/link';
 
 function MobileMenu({ items, isSpanish }: any) {
-  const [isOpen, setIsOpen] = React.useState(false);
-
-  const handleOpenChange = (open: boolean) => {
-    setIsOpen(open);
-    if (open) {
-      // When the sheet is opening, scroll to the top (fixes iOS issue)
-      window.scrollTo(0, 0);
-    }
-  };
   return (
-    <Sheet open={isOpen} onOpenChange={handleOpenChange}>
-      <SheetTrigger className="md:hidden text-white inline-flex items-center justify-center h-10 w-10">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="1.5rem"
-          height="1.5rem"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="feather feather-menu">
-          <line x1="3" y1="12" x2="21" y2="12"></line>
-          <line x1="3" y1="6" x2="21" y2="6"></line>
-          <line x1="3" y1="18" x2="21" y2="18"></line>
-        </svg>
+    <Sheet>
+      <SheetTrigger asChild>
+        <button className="md:hidden text-white inline-flex items-center justify-center h-10 w-10">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.5rem"
+            height="1.5rem"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="feather feather-menu">
+            <line x1="3" y1="12" x2="21" y2="12"></line>
+            <line x1="3" y1="6" x2="21" y2="6"></line>
+            <line x1="3" y1="18" x2="21" y2="18"></line>
+          </svg>
+        </button>
       </SheetTrigger>
       <SheetContent className="min-h-svh">
         <SheetHeader>
@@ -63,13 +56,6 @@ function MobileMenu({ items, isSpanish }: any) {
               </li>
             ))}
           </ul>
-
-          {/* <Link
-            variant="link"
-            href="/about"
-            className="text-lg font-medium hover:underline px-0 justify-start">
-            About
-          </Link> */}
         </nav>
       </SheetContent>
     </Sheet>
