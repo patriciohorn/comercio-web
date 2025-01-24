f140d6af8358dd33b126c5f306fa032de1419d48
Patricio Mena <luispatricio.mh@gmail.com>
2025-01-23 20:35:19 -0600

    Test mobile menu

--- src/components/mobileMenu.tsx
+++ src/components/mobileMenu.tsx
@@ -23,7 +23,7 @@ function MobileMenu({ items, isSpanish }: any) {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="feather feather-menu block overflow-visible">
+          className="feather feather-menu">
           <line x1="3" y1="12" x2="21" y2="12"></line>
           <line x1="3" y1="6" x2="21" y2="6"></line>
           <line x1="3" y1="18" x2="21" y2="18"></line>
